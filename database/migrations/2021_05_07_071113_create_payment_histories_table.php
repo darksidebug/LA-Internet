8795966ff904668ca3b13c7f897ebe4343c936e2
Miyunecadz <jvcadz@gmail.com>
2021-05-07 07:26:57 +0800

    Add migrations to plans, customers, payment_histories

---
+++ database/migrations/2021_05_07_071113_create_payment_histories_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePaymentHistoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payment_histories', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('account_number')->constrained('customers')->onDelete('cascade');
+            $table->date('date_of_payment');
+            $table->double('amount');
+            $table->double('remaining_balance');
+            $table->double('advanced_payment');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payment_histories');
+    }
+}
